d0eafb775e4a0471ba7e4d67794a9749df171fb5
Crankshaft Robot <robot+crankshaft@gocardless.com>
2018-03-16 16:18:53 +0000

    Crankshaft[BU0000056H361F] .NET client @ 2018-03-16 16:18:53 UTC

--- GoCardless/GoCardlessClient.cs
+++ GoCardless/GoCardlessClient.cs
@@ -213,9 +213,9 @@ namespace GoCardless
             var httpMethod = new HttpMethod(method);
 
             var requestMessage = new HttpRequestMessage(httpMethod, new Uri(_baseUrl, path));
-            requestMessage.Headers.Add("User-Agent", "gocardless-dotnet/2.4.0");
+            requestMessage.Headers.Add("User-Agent", "gocardless-dotnet/2.5.0");
             requestMessage.Headers.Add("GoCardless-Version", "2015-07-06");
-            requestMessage.Headers.Add("GoCardless-Client-Version", "2.4.0");
+            requestMessage.Headers.Add("GoCardless-Client-Version", "2.5.0");
             requestMessage.Headers.Add("GoCardless-Client-Library", "gocardless-dotnet");
             requestMessage.Headers.Authorization =
                 new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _accessToken);
