81a6011ef04a142349035436ae8ab636709252bf
Crankshaft Robot <robot+crankshaft@gocardless.com>
2018-04-11 12:14:18 +0000

    Crankshaft[BU000005C8THQM] .NET client @ 2018-04-11 12:14:18 UTC

--- GoCardless/Services/MandateService.cs
+++ GoCardless/Services/MandateService.cs
@@ -258,7 +258,7 @@ namespace GoCardless.Services
         /// <a name="mandates_scheme"></a>Direct Debit scheme to which this
         /// mandate and associated payments are submitted. Can be supplied or
         /// automatically detected from the customer's bank account. Currently
-        /// "autogiro", "bacs", "betalingsservice", and "sepa_core" are
+        /// "autogiro", "bacs", "becs", "betalingsservice", and "sepa_core" are
         /// supported.
         /// </summary>
         [JsonProperty("scheme")]
