b51148b7b1ec9608c737d73c0ab2be372f947be5
Crankshaft Robot <robot+crankshaft@gocardless.com>
2018-08-22 15:04:17 +0000

    Crankshaft[BU0000066P9J70] .NET client @ 2018-08-22 15:04:17 UTC

--- GoCardless/Services/CustomerNotificationService.cs
+++ GoCardless/Services/CustomerNotificationService.cs
@@ -26,6 +26,12 @@ namespace GoCardless.Services
     /// actioned in some
     /// way, it is no longer visible using this API.
     /// 
+    /// <p class="restricted-notice"><strong>Restricted</strong>: This API is
+    /// currently
+    /// only available for approved integrators - please <a
+    /// href="mailto:help@gocardless.com">get
+    /// in touch</a> if you would like to use this API.</p>
+    /// 
     /// </summary>
 
     public class CustomerNotificationService
