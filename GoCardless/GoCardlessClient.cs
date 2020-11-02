fb4da36a7d39f5d8c84859f8934024f1e9f2bac5
Robot <robot@gocardless.com>
2020-11-02 12:12:39 +0000

    Minor version bump to 4.5.0

--- GoCardless/GoCardlessClient.cs
+++ GoCardless/GoCardlessClient.cs
@@ -252,9 +252,9 @@ namespace GoCardless
             var httpMethod = new HttpMethod(method);
 
             var requestMessage = new HttpRequestMessage(httpMethod, new Uri(_baseUrl, path));
-            requestMessage.Headers.Add("User-Agent", "gocardless-dotnet/4.4.0");
+            requestMessage.Headers.Add("User-Agent", "gocardless-dotnet/4.5.0");
             requestMessage.Headers.Add("GoCardless-Version", "2015-07-06");
-            requestMessage.Headers.Add("GoCardless-Client-Version", "4.4.0");
+            requestMessage.Headers.Add("GoCardless-Client-Version", "4.5.0");
             requestMessage.Headers.Add("GoCardless-Client-Library", "gocardless-dotnet");
             requestMessage.Headers.Authorization =
                 new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _accessToken);
