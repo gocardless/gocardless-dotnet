a04c26317a06f0865c4903c3b2994e30bfa90c1d
Robot <robot@gocardless.com>
2022-08-02 10:23:25 +0000

    Changes generated by 2c5833ce5d3ecad2831f715a4c43589553cd71d1

--- GoCardless/Resources/BillingRequest.cs
+++ GoCardless/Resources/BillingRequest.cs
@@ -417,8 +417,11 @@ namespace GoCardless.Resources
         public IDictionary<string, string> Metadata { get; set; }
 
         /// <summary>
-        /// A Direct Debit scheme. Currently "ach", "bacs", "becs", "becs_nz",
-        /// "betalingsservice", "pad", "pay_to" and "sepa_core" are supported.
+        /// A Direct Debit scheme. Currently "ach", "autogiro", "bacs", "becs",
+        /// "becs_nz", "betalingsservice", "pad", "pay_to" and "sepa_core" are
+        /// supported. Optional for mandate only requests - if left blank, the
+        /// payer will be able to select the currency/scheme to pay with from a
+        /// list of your available schemes.
         /// </summary>
         [JsonProperty("scheme")]
         public string Scheme { get; set; }
