a64c66f3be68161304b15287e0a69fd4a8c2923f
Robot <robot@gocardless.com>
2022-09-29 08:21:40 +0000

    Changes generated by 6c3c4f420fd27e261dd24be6e01360167d463539

--- GoCardless/Services/BillingRequestTemplateService.cs
+++ GoCardless/Services/BillingRequestTemplateService.cs
@@ -222,8 +222,11 @@ namespace GoCardless.Services
         public IDictionary<String, String> MandateRequestMetadata { get; set; }
 
         /// <summary>
-        /// A Direct Debit scheme. Currently "ach", "bacs", "becs", "becs_nz",
-        /// "betalingsservice", "pad", "pay_to" and "sepa_core" are supported.
+        /// A Direct Debit scheme. Currently "ach", "autogiro", "bacs", "becs",
+        /// "becs_nz", "betalingsservice", "pad", "pay_to" and "sepa_core" are
+        /// supported. Optional for mandate only requests - if left blank, the
+        /// payer will be able to select the currency/scheme to pay with from a
+        /// list of your available schemes.
         /// </summary>
         [JsonProperty("mandate_request_scheme")]
         public string MandateRequestScheme { get; set; }
@@ -404,8 +407,11 @@ namespace GoCardless.Services
         public IDictionary<String, String> MandateRequestMetadata { get; set; }
 
         /// <summary>
-        /// A Direct Debit scheme. Currently "ach", "bacs", "becs", "becs_nz",
-        /// "betalingsservice", "pad", "pay_to" and "sepa_core" are supported.
+        /// A Direct Debit scheme. Currently "ach", "autogiro", "bacs", "becs",
+        /// "becs_nz", "betalingsservice", "pad", "pay_to" and "sepa_core" are
+        /// supported. Optional for mandate only requests - if left blank, the
+        /// payer will be able to select the currency/scheme to pay with from a
+        /// list of your available schemes.
         /// </summary>
         [JsonProperty("mandate_request_scheme")]
         public string MandateRequestScheme { get; set; }
