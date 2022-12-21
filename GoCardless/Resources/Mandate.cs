fc32bff5eebe1d0cdec647a7cede4d149c13fd83
Robot <robot@gocardless.com>
2022-12-21 16:25:22 +0000

    Changes generated by 47cadd9e482c18b4115b575859a0fcc9815b69aa

--- GoCardless/Resources/Mandate.cs
+++ GoCardless/Resources/Mandate.cs
@@ -20,6 +20,22 @@ namespace GoCardless.Resources
     public class Mandate
     {
         /// <summary>
+        /// This field is ACH specific, sometimes referred to as [SEC
+        /// code](https://www.moderntreasury.com/learn/sec-codes).
+        /// 
+        /// This is the way that the payer gives authorisation to the merchant.
+        ///   web: Authorisation is Internet Initiated or via Mobile Entry (maps
+        /// to SEC code: WEB)
+        ///   telephone: Authorisation is provided orally over telephone (maps
+        /// to SEC code: TEL)
+        ///   paper: Authorisation is provided in writing and signed, or
+        /// similarly authenticated (maps to SEC code: PPD)
+        /// 
+        /// </summary>
+        [JsonProperty("authorisation_source")]
+        public MandateAuthorisationSource? AuthorisationSource { get; set; }
+
+        /// <summary>
         /// (Optional) Payto and VRP Scheme specific information
         /// </summary>
         [JsonProperty("consent_parameters")]
@@ -111,6 +127,34 @@ namespace GoCardless.Resources
         public MandateStatus? Status { get; set; }
     }
     
+    /// <summary>
+    /// This field is ACH specific, sometimes referred to as [SEC
+    /// code](https://www.moderntreasury.com/learn/sec-codes).
+    /// 
+    /// This is the way that the payer gives authorisation to the merchant.
+    ///   web: Authorisation is Internet Initiated or via Mobile Entry (maps to SEC code: WEB)
+    ///   telephone: Authorisation is provided orally over telephone (maps to SEC code: TEL)
+    ///   paper: Authorisation is provided in writing and signed, or similarly authenticated (maps
+    /// to SEC code: PPD)
+    /// 
+    /// </summary>
+    [JsonConverter(typeof(GcStringEnumConverter), (int)Unknown)]
+    public enum MandateAuthorisationSource {
+        /// <summary>Unknown status</summary>
+        [EnumMember(Value = "unknown")]
+        Unknown = 0,
+
+        /// <summary>`authorisation_source` with a value of "web"</summary>
+        [EnumMember(Value = "web")]
+        Web,
+        /// <summary>`authorisation_source` with a value of "telephone"</summary>
+        [EnumMember(Value = "telephone")]
+        Telephone,
+        /// <summary>`authorisation_source` with a value of "paper"</summary>
+        [EnumMember(Value = "paper")]
+        Paper,
+    }
+
     /// <summary>
     /// Represents a mandate consent parameter resource.
     ///
