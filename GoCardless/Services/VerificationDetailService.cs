8b78ef43fe4285c102efaeb2861d790beae599d3
Robot <robot@gocardless.com>
2023-01-30 12:47:45 +0000

    Changes generated by 0e4a8347da6b24e937b6b8a72bd39d4383c32ddf

--- GoCardless/Services/VerificationDetailService.cs
+++ GoCardless/Services/VerificationDetailService.cs
@@ -35,7 +35,7 @@ namespace GoCardless.Services
         /// <summary>
         /// Verification details represent any information needed by GoCardless
         /// to verify a creditor.
-        /// Currently, only UK-based companies are supported. 
+        /// Currently, only UK-based companies are supported.
         /// In other words, to submit verification details for a creditor, their
         /// creditor_type must be company and their country_code must be GB.
         /// </summary>
@@ -57,7 +57,7 @@ namespace GoCardless.Services
     /// <summary>
     /// Verification details represent any information needed by GoCardless to
     /// verify a creditor.
-    /// Currently, only UK-based companies are supported. 
+    /// Currently, only UK-based companies are supported.
     /// In other words, to submit verification details for a creditor, their
     /// creditor_type must be company and their country_code must be GB.
     /// </summary>
