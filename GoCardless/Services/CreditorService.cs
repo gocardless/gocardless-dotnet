fbf4f0f8f02050e5f1253c02592b950312e952f5
Robot <robot@gocardless.com>
2023-03-27 16:41:14 +0000

    Changes generated by 5b48e23728eca84c0b23a9fa1a2ade48e8b42fb6

--- GoCardless/Services/CreditorService.cs
+++ GoCardless/Services/CreditorService.cs
@@ -153,11 +153,20 @@ namespace GoCardless.Services
         /// <summary>
         /// Applies a [scheme identifier](#core-endpoints-scheme-identifiers) to
         /// a creditor.
+        /// 
+        /// If the scheme identifier has a `pending` status, it will be applied
+        /// asynchronously
+        /// once it becomes `active`.
+        /// 
         /// If the creditor already has a scheme identifier for the scheme, it
         /// will be replaced,
-        /// and any mandates attached to it transferred asynchronously.
-        /// For some schemes, the application of the scheme identifier will be
-        /// performed asynchronously.
+        /// and any mandates attached to it transferred asynchronously. On Bacs
+        /// and SEPA, if
+        /// payments were about to be submitted, they will be delayed. To
+        /// minimise this delay, we
+        /// recommend that you apply the new scheme identifier after the daily
+        /// payment submission
+        /// time (4 PM Europe/London time).
         /// 
         /// </summary>  
         /// <param name="identity">Unique identifier, beginning with "CR".</param> 
@@ -446,11 +455,20 @@ namespace GoCardless.Services
     /// <summary>
     /// Applies a [scheme identifier](#core-endpoints-scheme-identifiers) to a
     /// creditor.
+    /// 
+    /// If the scheme identifier has a `pending` status, it will be applied
+    /// asynchronously
+    /// once it becomes `active`.
+    /// 
     /// If the creditor already has a scheme identifier for the scheme, it will
     /// be replaced,
-    /// and any mandates attached to it transferred asynchronously.
-    /// For some schemes, the application of the scheme identifier will be
-    /// performed asynchronously.
+    /// and any mandates attached to it transferred asynchronously. On Bacs and
+    /// SEPA, if
+    /// payments were about to be submitted, they will be delayed. To minimise
+    /// this delay, we
+    /// recommend that you apply the new scheme identifier after the daily
+    /// payment submission
+    /// time (4 PM Europe/London time).
     /// 
     /// </summary>
     public class CreditorApplySchemeIdentifierRequest
