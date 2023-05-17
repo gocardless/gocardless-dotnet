490b9bc29209efba46d3b53f195af934cbd214fc
Robot <robot@gocardless.com>
2023-05-17 13:47:42 +0000

    Changes generated by 7b4adc09d4eacd2b03fc03c591ac72db58169eeb

--- GoCardless/Resources/NegativeBalanceLimit.cs
+++ GoCardless/Resources/NegativeBalanceLimit.cs
@@ -20,12 +20,6 @@ namespace GoCardless.Resources
     public class NegativeBalanceLimit
     {
         /// <summary>
-        /// Whether or not this limit is currently active
-        /// </summary>
-        [JsonProperty("active")]
-        public bool? Active { get; set; }
-
-        /// <summary>
         /// The limit amount in pence (e.g. 10000 for a -100 GBP limit).
         /// </summary>
         [JsonProperty("balance_limit")]
